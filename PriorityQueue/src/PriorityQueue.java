e1aec41284ccf943137dcfd6eca5f9aec2b96b9e
WegielWBucie <pondroszejset@gmail.com>
2022-07-15 02:07:53 +0200

    >> a few more mods

--- PriorityQueue/src/PriorityQueue.java
+++ PriorityQueue/src/PriorityQueue.java
@@ -1,29 +1,49 @@
 import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.Collections;
 
 public class PriorityQueue {
 
     ArrayList<Node>queue;
-    private int filledElements;
+    private int sizeOfHeap;
     private static final int FRONT = 1;
 
     public PriorityQueue() {
-        queue = new ArrayList<>();
-        this.filledElements = 0;
+        queue = new ArrayList<>(1);
+        queue.add(0, new Node(Integer.MAX_VALUE, ""));
+        this.sizeOfHeap = 0;
     }
 
     public PriorityQueue(Integer minCapacity) {
-        queue = new ArrayList<>();
-        queue.ensureCapacity(minCapacity);
-        this.filledElements = 0;
+        queue = new ArrayList<>(minCapacity + 1);
+        queue.add(0, new Node(Integer.MAX_VALUE, ""));
+        this.sizeOfHeap = 0;
     }
 
     protected boolean notLeaf(int position) {
-        return position < (filledElements / 2) || position > filledElements;
+        return position < ((sizeOfHeap + 1) / 2) || position > sizeOfHeap;
     }
 
     public void heapify(Integer position) {
+        if(notLeaf(position)) {
+            if(queue.get(position).priority < queue.get(getLeftChildPosition(position)).priority ||
+                    queue.get(position).priority < queue.get(getRightChildPosition(position)).priority) {
 
+                if(queue.get(getLeftChildPosition(position)).priority > queue.get(getRightChildPosition(position)).priority) {
+                    swapNodes(position, getLeftChildPosition(position));
+                    heapify(position);
+                }
+                else {
+                    swapNodes(position, getRightChildPosition(position));
+                    heapify(position);
+                }
+            }
+        }
+    }
+
+    public void designQueue() {
+        for(int position = sizeOfHeap / 2; position >= 1; position--) {
+            heapify(position);
+        }
     }
 
     protected int getParentPosition(int position) {
@@ -36,10 +56,16 @@ public class PriorityQueue {
         return 2 * position + 1;
     }
 
+    public void swapNodes(int firstNode, int secondNode) {
+        Collections.swap(queue, firstNode, secondNode);
+    }
+
     public void enqueue(Node node) {
         queue.add(node);
-        filledElements++;
-        while(queue.get(filledElements++).priority )
+        sizeOfHeap++;
+        while(queue.get(sizeOfHeap).priority < queue.get(getParentPosition(sizeOfHeap)).priority) {
+            swapNodes(sizeOfHeap, getParentPosition(sizeOfHeap));
+        }
     }
 
     public void delete(int indexToRemove) {
